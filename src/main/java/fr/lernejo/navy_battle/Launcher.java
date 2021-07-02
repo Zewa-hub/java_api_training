5db42a04cff04d2da7e20e3597db3736d81f70b1
Abdelaziz Mansouri <abdelaziz.mansouri@epita.fr>
2021-07-02 21:21:51 +0200

    Correction of Launcher and Player bug

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -3,7 +3,7 @@ import com.sun.net.httpserver.HttpServer;
 
 public class Launcher {
     public static void main(String[] args) {
-        /*
+
         if (args.length == 1)
         {
             Server server =  new Server(Integer.parseInt(args[0]));
@@ -13,10 +13,6 @@ public class Launcher {
             Server server = new Server(Integer.parseInt(args[0]));
             server.start_game(args[1]);
         }
-        else throw new IllegalArgumentException("Please call Main method with a port number");
-    }*/
-        Server server = new Server(9876);
-        Server server2 = new Server(8765);
-        server2.start_game("http://localhost:9876");
+        else throw new IllegalArgumentException("Error with arguments");
     }
 }
