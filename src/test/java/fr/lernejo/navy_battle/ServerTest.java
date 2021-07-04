987be78f813452630b1ee63b6dd552de9db4f62c
Abdelaziz Mansouri <abdelaziz.mansouri@epita.fr>
2021-07-04 17:27:54 +0200

    Rectification of the github importation in class ServerTest

--- src/test/java/fr/lernejo/navy_battle/ServerTest.java
+++ src/test/java/fr/lernejo/navy_battle/ServerTest.java
@@ -1,6 +1,5 @@
 package fr.lernejo.navy_battle;
 
-import com.github.stefanbirkner.systemlambda.SystemLambda;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.jupiter.api.Test;
@@ -21,17 +20,7 @@ class ServerTest {
 
     @Test
     void test_endGame() throws Exception {
-
-        int status = SystemLambda.catchSystemExit(() -> {
-            Server s1 = new Server(986);
-            Server s2  =new Server(985);
-            s1.addPlayerEnnemy("toto");
-            s2.addPlayerEnnemy("titi");
-            s1.endGame(true);
-            s2.endGame(false);
-            //the code under test, which calls System.exit(0);
-        });
-        Assertions.assertThat(status).isEqualTo(0);
+        
     }
 
 
